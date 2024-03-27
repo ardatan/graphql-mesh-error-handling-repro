cc2db94ee40c6e76f482f4ed38dd22efa0d3be37
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-03-27 12:06:18 +0000

    Pending changes exported from your codespace

---
+++ .mesh/sources/FakeAPI/types.ts
@@ -0,0 +1,61 @@
+// @ts-nocheck
+
+import { InContextSdkMethod } from '@graphql-mesh/types';
+import { MeshContext } from '@graphql-mesh/runtime';
+
+export namespace FakeApiTypes {
+  export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+  /** The `JSON` scalar type represents JSON values as specified by [ECMA-404](http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf). */
+  JSON: { input: any; output: any; }
+  ObjMap: { input: any; output: any; }
+};
+
+export type Query = {
+  greeting?: Maybe<Scalars['JSON']['output']>;
+  throwMe?: Maybe<Scalars['String']['output']>;
+};
+
+export type HTTPMethod =
+  | 'GET'
+  | 'HEAD'
+  | 'POST'
+  | 'PUT'
+  | 'DELETE'
+  | 'CONNECT'
+  | 'OPTIONS'
+  | 'TRACE'
+  | 'PATCH';
+
+  export type QuerySdk = {
+      /** undefined **/
+  greeting: InContextSdkMethod<Query['greeting'], {}, MeshContext>,
+  /** undefined **/
+  throwMe: InContextSdkMethod<Query['throwMe'], {}, MeshContext>
+  };
+
+  export type MutationSdk = {
+    
+  };
+
+  export type SubscriptionSdk = {
+    
+  };
+
+  export type Context = {
+      ["FakeAPI"]: { Query: QuerySdk, Mutation: MutationSdk, Subscription: SubscriptionSdk },
+      
+    };
+}
---
+++ additionalResolvers.ts
@@ -0,0 +1,8 @@
+export default {
+    Query: {
+      throwMe() {
+        throw new Error('This is a test error');
+      },
+    },
+  };
+  
